bb837e4e39cff40e59351e1a5a0f0f04e9800313
ashish <ashishthomas1995@gmail.com>
2025-07-24 18:24:48 +0530

    updated routes using lazyloading and sidebar component

--- src/Routes/AdminRoutes.jsx
+++ src/Routes/AdminRoutes.jsx
@@ -1,61 +1,127 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
-import Dashboard from "../Admin/Dashboard/Dashboard";
-import Users from "../Admin/Users/Users";
-import CoreWatchlist from "../Admin/CoreWatchlist/CoreWatchlist";
-import Onboarding from "../Admin/OnBoardingChecklist/Onboarding";
-import MyBooks from "../Admin/Books/MyBooks";
-import SuccessStory from "../Admin/SuccessStories/SuccessStory";
-import Support from "../Admin/Support/Support";
-import References from "../Admin/References/References";
-import Profilemain from "../Admin/Profile/Profilemain";
-import ChangePassword from "../Admin/Profile/ChangePassword";
+import { lazy, Suspense } from "react";
+import { Box, CircularProgress } from "@mui/material";
+
+const App = lazy(() => import("../App"));
+const Dashboard = lazy(() => import("../Admin/Dashboard/Dashboard"));
+const Users = lazy(() => import("../Admin/Users/Users"));
+const CoreWatchlist = lazy(() =>
+  import("../Admin/CoreWatchlist/CoreWatchlist")
+);
+const Onboarding = lazy(() =>
+  import("../Admin/OnBoardingChecklist/Onboarding")
+);
+
+const SuccessStory = lazy(() => import("../Admin/SuccessStories/SuccessStory"));
+const Support = lazy(() => import("../Admin/Support/Support"));
+const References = lazy(() => import("../Admin/References/References"));
+const Profilemain = lazy(() => import("../Admin/Profile/Profilemain"));
+const ChangePassword = lazy(() => import("../Admin/Profile/ChangePassword"));
+
+const MuiLoader = () => (
+  <Box
+    sx={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      minHeight: "40vh",
+    }}
+  >
+    <CircularProgress color="primary" size={60} thickness={4} />
+  </Box>
+);
+
+const MyBooks = lazy(() => import("../Admin/Books/MyBooks"));
 
 export const Adminapproute = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: (
+      <Suspense fallback={<MuiLoader />}>
+        <App />
+      </Suspense>
+    ),
     children: [
       {
         path: "/",
-        element: <Dashboard />,
+        element: (
+          <Suspense fallback={<MuiLoader />}>
+            <Dashboard />
+          </Suspense>
+        ),
       },
       {
         path: "users",
-        element: <Users />,
+        element: (
+          <Suspense fallback={<MuiLoader />}>
+            <Users />
+          </Suspense>
+        ),
       },
       {
         path: "core",
-        element: <CoreWatchlist />,
+        element: (
+          <Suspense fallback={<MuiLoader />}>
+            <CoreWatchlist />
+          </Suspense>
+        ),
       },
       {
         path: "onboarding",
-        element: <Onboarding />,
+        element: (
+          <Suspense fallback={<MuiLoader />}>
+            <Onboarding />
+          </Suspense>
+        ),
       },
       {
         path: "book",
-        element: <MyBooks />,
+        element: (
+          <Suspense fallback={<MuiLoader />}>
+            <MyBooks />
+          </Suspense>
+        ),
       },
       {
         path: "success",
-        element: <SuccessStory />,
+        element: (
+          <Suspense fallback={<MuiLoader />}>
+            <SuccessStory />
+          </Suspense>
+        ),
       },
       {
         path: "support",
-        element: <Support />,
+        element: (
+          <Suspense fallback={<MuiLoader />}>
+            <Support />
+          </Suspense>
+        ),
       },
       {
         path: "references",
-        element: <References />,
+        element: (
+          <Suspense fallback={<MuiLoader />}>
+            <References />
+          </Suspense>
+        ),
       },
     ],
   },
   {
     path: "profilemain",
-    element: <Profilemain />,
+    element: (
+      <Suspense fallback={<MuiLoader />}>
+        <Profilemain />
+      </Suspense>
+    ),
   },
   {
     path: "changepassword",
-    element: <ChangePassword />,
+    element: (
+      <Suspense fallback={<MuiLoader />}>
+        <ChangePassword />
+      </Suspense>
+    ),
   },
 ]);
